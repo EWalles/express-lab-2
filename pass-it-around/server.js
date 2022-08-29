24b6bbcf9d49bea09262b235b583a689c7959502
EWalles <ejomcdonald@gmail.com>
2022-08-29 18:50:31 -0400

    bottles on the wall

--- pass-it-around/server.js
+++ pass-it-around/server.js
@@ -13,7 +13,13 @@ app.get("/", (req, res) => {
   console.log("listening")
   res.send("99 Bottles of beer on the wall")
 })
-app.get("/:numbers_of_bottle", (req, res) => {
-    console.log(req.params)
-    res.send("take one down")
+app.get("/:numberOfBottles?", function( req, res ){
+    var numberOfBottles = req.params.numberOfBottles || 99;
+    var next = numberOfBottles - 1;
+    if (numberOfBottles > 1){
+      res.send(numberOfBottles + " bottles of beer on the wall <a href='/" + next + "'>Take one down pass it around");
+    }
+    else{
+      res.send("1 bottle of beer on the wall <a href='/'>Start Over</a>");
+    }
   });
