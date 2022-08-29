6959b8e8ff7b594de31f33cd59e5a452470c700e
EWalles <ejomcdonald@gmail.com>
2022-08-29 17:45:11 -0400

    greeting express application created

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ const express = require("express");
 const app = express();
 
 //port
-const port = 3000
+const port = 3000;
 
 // define routes req=request res=response
 
-app.get("/greeting/:firstname/:lastname", (req, res) => {
-    console.log(req.params)
-    res.send("Hello" + req.params.firstname + " " + req.params.lastname)
-  })
-  
+app.get("/hello/:firstname/:lastname", (req, res) => {
+  console.log(req.params)
+  res.send("hello " + req.params.firstname + " " + req.params.lastname)
+})
+
 // tell the app to listen on port...
-app.listen(3000, function(){
-    console.log()
+app.listen(port, function(){
+    console.log("listening")
 });
