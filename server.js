53d9968a26f57ca2487f376eb1a82e60f8fa97b6
EWalles <ejomcdonald@gmail.com>
2022-08-29 18:41:58 -0400

    8ball

--- server.js
+++ server.js
@@ -18,6 +18,23 @@ app.get("/tip/:total/:tipPrecentage", (req, res) => {
   console.log(req.params)
   res.send("Your tip " + (req.params.total) * (req.params.tipPrecentage /100))
 });
+let ball = [
+  "It is certain", "It is decidedly so", 
+  "Without a doubt", "Yes definitely",
+  "You may rely on it", "As I see it yes", 
+  "Most likely", "Outlook good",
+  "Yes", "Signs point to yes", 
+  "Reply hazy try again", "Ask again later",
+  "Better not tell you now", "Cannot predict now", 
+  "Concentrate and ask again","Don't count on it", 
+  "My reply is no", "My sources say no",
+  "Outlook not so good", "Very doubtful"]
+
+  let answers = ball[Math.floor(math.random()*ball.length)];
+
+app.get("/magic/:answer", (req, res) => 
+{res.send(<h1>${answer}</h1>)});
+
 
 // tell the app to listen on port...
 app.listen(port, function(){
