94fff48a2a364d14957352aafa1978e1681c5640
EWalles <ejomcdonald@gmail.com>
2022-08-29 21:10:00 -0400

    update

--- pass-it-around/server.js
+++ pass-it-around/server.js
@@ -17,8 +17,9 @@ app.get("/", function(req,res){
     var numberOfBottles = req.params.numberOfBottles || 99;
     var next = numberOfBottles - 1;
     if (numberOfBottles > 1){
-      res.send(numberOfBottles + " bottles of beer on the wall <a href='/"> + next +  "Take one down pass it around");
+      res.send(numberOfBottles + " bottles of beer on the wall! Take one down pass it around");
     }
+  
     else{
       res.send("1 bottle of beer on the wall <a href='/'> Start Over </a>");
     }
